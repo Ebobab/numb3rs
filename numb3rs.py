e82036edaca8bb6823ebeae5aafd77c13a8ed78a
Ebobab <edwardsan@web.de>
2025-07-02 04:10:03 +0200

    Update numb3rs.py
    beautified

--- numb3rs.py
+++ numb3rs.py
@@ -3,21 +3,15 @@ import sys
 
 
 def main():
-    
-    print(validate(input("IPv4 Address: ")))
-
+    user_input = input("IPv4 Adress: "
+    validated = validate(user_input)
+    print(validated)
 
 def validate(ip):
-    
     if re.search(r"^((25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})\.){3}(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})$", ip):
         return True
     else:
         return False
-
-    
-
-
-...
-
+        
 if __name__ == "__main__":
     main()
